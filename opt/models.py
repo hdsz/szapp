900243416ac487fa434eaaec2d4b8d9b86dd94aa
hdsz <40573725+hdsz@users.noreply.github.com>
2019-03-14 18:13:43 +0100

    comments addin

--- opt/models.py
+++ opt/models.py
@@ -40,13 +40,31 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
+class Comment(db.Model):
+    __tablename__='comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+   
+   #foreign key
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
 
+    def __repr__(self):
+        return f"Comment('{self.body}', '{self.timestamp}')"from datetime import datetime
+   
+   
+   
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+      
+      
+    #relationship
+    comments=db.relationship('Comment',backref='post',lazy=True)
+   
     #foreign key
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
